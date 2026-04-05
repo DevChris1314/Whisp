6eae4ef682e57e06b4a38b7f53bc013be599d5b7
Cristian Molina <devchris1314@gmail.com>
2026-04-04 21:47:34 -0400

    Initial Commit

---
+++ Whisp/WhispApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhispApp.swift
+//  Whisp
+//
+//  Created by Cristian Molina on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct WhispApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhispTests/WhispTests.swift
@@ -0,0 +1,18 @@
+//
+//  WhispTests.swift
+//  WhispTests
+//
+//  Created by Cristian Molina on 4/4/26.
+//
+
+import Testing
+
+struct WhispTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
